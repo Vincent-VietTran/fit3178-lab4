78be8fdf3e6d9dfe344fe3acfaf37b8c1bfdde6d
Viet Tran <vtra0041@student.monash.edu>
2025-08-13 13:40:21 +1000

    feat: Define Superhero class and Design base UI for Current Party table view controller

--- FIT3178-W03-Lab/Superhero.swift
+++ FIT3178-W03-Lab/Superhero.swift
@@ -7,6 +7,22 @@
 
 import UIKit
 
-class Superhero: NSObject {
+// Universe enum
+enum Universe: Int {
+case marvel = 0
+case dc = 1
+}
 
+class Superhero: NSObject {
+    // Why optional, research on persistent data
+    var name: String?;
+    var abilities: String?;
+    var universe: Universe?;
+    
+    init(name: String, abilities: String, universe: Universe){
+        self.name = name;
+        self.abilities = abilities;
+        self.universe = universe;
+    }
+    
 }
