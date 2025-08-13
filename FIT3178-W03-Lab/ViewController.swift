6f9aee446a09f14a34c08590c22613dcb9065150
Viet Tran <vtra0041@student.monash.edu>
2025-08-13 13:16:50 +1000

    Initial Commit

---
+++ FIT3178-W03-Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIT3178-W03-Lab
+//
+//  Created by Viet Tran on 13/8/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
